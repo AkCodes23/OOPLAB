0f1438d1cd9e901ca48440bbd11bb2235e326eec
Akhil Varanasi <135016848+AkCodes23@users.noreply.github.com>
2024-08-03 09:10:29 +0530

    Update Q1.java

--- Lab 2/Q1.java
+++ Lab 2/Q1.java
@@ -1,7 +1,7 @@
 // LeapYearCheck.java
 import java.util.Scanner;
 
-public class LeapYearCheck {
+class LeapYearCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a year: ");
