3e8c1499a99c0a6724619d031fd08de09dd1d3c0
Akhil Varanasi <135016848+AkCodes23@users.noreply.github.com>
2024-08-03 10:13:50 +0530

    Add files via upload

---
+++ Lab 2/AQ3.java
@@ -0,0 +1,11 @@
+class AQ3 {
+    public static void main(String[] args) {
+        // Statement a
+        boolean x = true;
+        System.out.println("Boolean x: " + x);
+
+        // Statement b with conversion
+        int y = x ? 1 : 0; // Convert boolean to int
+        System.out.println("Integer y: " + y);
+    }
+}
