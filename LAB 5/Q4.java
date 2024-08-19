dc3b76a379575a98acabf35494d538c350b0ebdd
Akhil Varanasi <135016848+AkCodes23@users.noreply.github.com>
2024-08-20 00:04:01 +0530

    Create Q4.java

---
+++ LAB 5/Q4.java
@@ -0,0 +1,40 @@
+class Complex {
+    int real;
+    int imaginary;
+
+    Complex() {
+        real = 0;
+        imaginary = 0;
+    }
+
+    Complex(int r, int i) {
+        real = r;
+        imaginary = i;
+    }
+
+    
+    Complex add(int value) {
+        return new Complex(real + value, imaginary);
+    }
+
+    
+    Complex add(Complex c) {
+        return new Complex(this.real + c.real, this.imaginary + c.imaginary);
+    }
+
+   
+    void display() {
+        System.out.println(real + " + " + imaginary + "i");
+    }
+
+    public static void main(String[] args) {
+        Complex c1 = new Complex(5, 7);
+        Complex c2 = new Complex(3, 4);
+        
+        Complex result1 = c1.add(10);
+        result1.display();
+        
+        Complex result2 = c1.add(c2);
+        result2.display();
+    }
+}
