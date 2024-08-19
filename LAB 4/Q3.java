a5b4ac9f947faa909a5937130dee00090ab3227d
Akhil Varanasi <135016848+AkCodes23@users.noreply.github.com>
2024-08-19 23:42:48 +0530

    Create Q3.java

---
+++ LAB 4/Q3.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class SymmetricMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the square matrix: ");
+        int n = sc.nextInt();
+        int[][] matrix = new int[n][n];
+
+        System.out.println("Enter the elements of the matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        boolean isSymmetric = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] != matrix[j][i]) {
+                    isSymmetric = false;
+                    break;
+                }
+            }
+        }
+
+        if (isSymmetric) {
+            System.out.println("The matrix is symmetric.");
+        } else {
+            System.out.println("The matrix is not symmetric.");
+        }
+    }
+}
