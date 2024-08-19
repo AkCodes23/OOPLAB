e66692a638c8ae02ae200f7cab3f8b06c20fef4b
Akhil Varanasi <135016848+AkCodes23@users.noreply.github.com>
2024-08-19 23:48:59 +0530

    Create AQ2.java

---
+++ LAB 4/AQ2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class LargestAndSmallestInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = arr[0];
+        int smallest = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+
+        System.out.println("Largest element: " + largest);
+        System.out.println("Smallest element: " + smallest);
+    }
+}
