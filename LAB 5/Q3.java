fb029b015985c1a34c53e4eba96c96065e8cc975
Akhil Varanasi <135016848+AkCodes23@users.noreply.github.com>
2024-08-31 09:34:15 +0530

    Update Q3.java

--- LAB 5/Q3.java
+++ LAB 5/Q3.java
@@ -1,28 +1,26 @@
+import java.util.Scanner;
+
 class Time {
     int hours;
     int minutes;
     int seconds;
 
-   
     Time() {
         hours = 0;
         minutes = 0;
         seconds = 0;
     }
 
-    
     Time(int h, int m, int s) {
         hours = h;
         minutes = m;
         seconds = s;
     }
 
-   
     void displayTime() {
         System.out.printf("%02d:%02d:%02d\n", hours, minutes, seconds);
     }
 
-    
     Time add(Time t) {
         int totalSeconds = this.seconds + t.seconds;
         int totalMinutes = this.minutes + t.minutes + totalSeconds / 60;
@@ -32,10 +30,31 @@ class Time {
     }
 
     public static void main(String[] args) {
-        Time t1 = new Time(1, 45, 30);
-        Time t2 = new Time(2, 30, 45);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first time:");
+        System.out.print("Hours: ");
+        int hours1 = scanner.nextInt();
+        System.out.print("Minutes: ");
+        int minutes1 = scanner.nextInt();
+        System.out.print("Seconds: ");
+        int seconds1 = scanner.nextInt();
+
+        System.out.println("Enter second time:");
+        System.out.print("Hours: ");
+        int hours2 = scanner.nextInt();
+        System.out.print("Minutes: ");
+        int minutes2 = scanner.nextInt();
+        System.out.print("Seconds: ");
+        int seconds2 = scanner.nextInt();
+
+        Time t1 = new Time(hours1, minutes1, seconds1);
+        Time t2 = new Time(hours2, minutes2, seconds2);
 
         Time t3 = t1.add(t2);
+        System.out.print("Total time: ");
         t3.displayTime();
+
+        scanner.close();
     }
 }
