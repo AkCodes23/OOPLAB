ca4e1877bb4e66cb26ec44bdeb29fdee2c137a31
Akhil Varanasi <135016848+AkCodes23@users.noreply.github.com>
2024-08-31 09:31:28 +0530

    Update Q1.java

--- LAB 5/Q1.java
+++ LAB 5/Q1.java
@@ -3,20 +3,21 @@ class Box {
     double height;
     double depth;
 
-    // Parameterized constructor to initialize dimensions
-    Box(double w, double h, double d) {
+    void dim(double w, double h, double d) {
         width = w;
         height = h;
         depth = d;
     }
 
-    // Method to calculate and return volume
     double getVolume() {
         return width * height * depth;
     }
+}
 
+class Test {
     public static void main(String[] args) {
-        Box myBox = new Box(5.0, 6.0, 7.0);
+        Box myBox = new Box();
+        myBox.dim(5.0, 6.0, 7.0);
         System.out.println("Volume of the box: " + myBox.getVolume());
     }
 }
