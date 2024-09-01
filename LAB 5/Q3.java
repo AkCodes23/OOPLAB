c663078541e87753eb15368ed79a0d3d74455a12
Akhil Varanasi <135016848+AkCodes23@users.noreply.github.com>
2024-09-01 16:34:00 +0530

    Update Q3.java

--- LAB 5/Q3.java
+++ LAB 5/Q3.java
@@ -1,55 +1,55 @@
 import java.util.Scanner;
 
 class Time {
-    int hours;
-    int minutes;
-    int seconds;
+    int hr;
+    int min;
+    int sec;
 
     Time() {
-        hours = 0;
-        minutes = 0;
-        seconds = 0;
+        hr = 0;
+        min = 0;
+        sec = 0;
     }
 
     Time(int h, int m, int s) {
-        hours = h;
-        minutes = m;
-        seconds = s;
+        hr = h;
+        min = m;
+        sec = s;
     }
 
     void displayTime() {
-        System.out.printf("%02d:%02d:%02d\n", hours, minutes, seconds);
+        System.out.printf("%02d:%02d:%02d\n", hr, min, sec);
     }
 
     Time add(Time t) {
-        int totalSeconds = this.seconds + t.seconds;
-        int totalMinutes = this.minutes + t.minutes + totalSeconds / 60;
-        int totalHours = this.hours + t.hours + totalMinutes / 60;
+        int totalsec = this.sec + t.sec;
+        int totalmin = this.min + t.min + totalsec / 60;
+        int totalhr = this.hr + t.hr + totalmin / 60;
 
-        return new Time(totalHours % 24, totalMinutes % 60, totalSeconds % 60);
+        return new Time(totalhr % 24, totalmin % 60, totalsec % 60);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter first time:");
-        System.out.print("Hours: ");
-        int hours1 = scanner.nextInt();
-        System.out.print("Minutes: ");
-        int minutes1 = scanner.nextInt();
-        System.out.print("Seconds: ");
-        int seconds1 = scanner.nextInt();
+        System.out.print("hr: ");
+        int hr1 = scanner.nextInt();
+        System.out.print("min: ");
+        int min1 = scanner.nextInt();
+        System.out.print("sec: ");
+        int sec1 = scanner.nextInt();
 
         System.out.println("Enter second time:");
-        System.out.print("Hours: ");
-        int hours2 = scanner.nextInt();
-        System.out.print("Minutes: ");
-        int minutes2 = scanner.nextInt();
-        System.out.print("Seconds: ");
-        int seconds2 = scanner.nextInt();
+        System.out.print("hr: ");
+        int hr2 = scanner.nextInt();
+        System.out.print("min: ");
+        int min2 = scanner.nextInt();
+        System.out.print("sec: ");
+        int sec2 = scanner.nextInt();
 
-        Time t1 = new Time(hours1, minutes1, seconds1);
-        Time t2 = new Time(hours2, minutes2, seconds2);
+        Time t1 = new Time(hr1, min1, sec1);
+        Time t2 = new Time(hr2, min2, sec2);
 
         Time t3 = t1.add(t2);
         System.out.print("Total time: ");
